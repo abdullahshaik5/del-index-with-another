e1bf03206a4b23fee5d3c6e5d33342d1bb62bd09
abdullahshaik5 <66948819+abdullahshaik5@users.noreply.github.com>
2022-11-04 14:17:55 +0530

    Create call.py

---
+++ call.py
@@ -0,0 +1,16 @@
+def ver():
+    print("im ver")
+def se():
+    print("im se")
+def test():
+    print("im test")
+def fwc():
+    print("im fwc")
+def simics():
+    print("im simics")
+def env():
+    print("im env")
+def pyscript():
+    print("im pyscript")
+
+#call a function from terminal
